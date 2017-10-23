4b204d3431c09b069b36377a8e89b7e42b9476a5
Abe Haskins <abeisgreat@abeisgreat.com>
2017-10-23 14:08:31 -0700

    Cleans up code

--- js/functions/index.js
+++ js/functions/index.js
@@ -20,68 +20,75 @@ const Translate = require('@google-cloud/translate');
 const translate = Translate({keyFilename: "service-account-credentials.json"});
 const Encoding = Speech.v1.types.RecognitionConfig.AudioEncoding;
 const Firestore = require('@google-cloud/firestore');
+const getLanguageWithoutLocale = require("./utils").getLanguageWithoutLocale;
 
 const db = new Firestore();
-
-function getLanguageWithoutLocale(languageCode) {
-    if (languageCode.indexOf("-") >= 0) {
-        return languageCode.substring(0, languageCode.indexOf("-"));
-    }
-    return languageCode;
-}
 
 exports.onUploadFS = functions.firestore
     .document("/uploads/{uploadId}")
     .onWrite((event) => {
-        let data = event.data.data();
-        console.log(data);
-        let language = data.language ? data.language : "en";
-        let sampleRate = data.sampleRate ? parseInt(data.sampleRate, 10) : 16000;
-        let encoding = data.encoding == "FLAC" ? Encoding.FLAC : Encoding.LINEAR16;
+        const data = event.data.data();
+        const languageCode = data.language ? data.language : "en";
+        const sampleRateHertz = data.sampleRate ? parseInt(data.sampleRate, 10) : 16000;
+        const encoding = data.encoding == "AMR" ? Encoding.AMR : Encoding.LINEAR16;
+        const uri = `gs://${process.env.GCP_PROJECT}.appspot.com/${data.fullPath}`;
 
-        let request = {
+        const request = {
             config: {
-                languageCode : language,
-                sampleRateHertz : sampleRate,
-                encoding : encoding
+                languageCode,
+                sampleRateHertz,
+                encoding
             },
-            audio: { uri : `gs://${process.env.GCP_PROJECT}.appspot.com/${data.fullPath}` }
+            audio: { uri }
         };
 
-        console.log(request);
-
         return speech.recognize(request).then((response) => {
-            let transcript = response[0].results[0].alternatives[0].transcript;
-            return db.collection("transcripts").doc(event.params.uploadId).set({text: transcript, language: language});
+            const text = response[0].results[0].alternatives[0].transcript;
+
+            return db.collection("transcripts")
+                .doc(event.params.uploadId)
+                .set({
+                    text, 
+                    language
+                });
         });
     });
 
 exports.onTranscriptFS = functions.firestore
     .document("/transcripts/{transcriptId}")
     .onWrite((event) => {
-        let value = event.data.data();
-        let transcriptId = event.params.transcriptId;
-        let text = value.text ? value.text : value;
-        let languages = ["en", "es", "pt", "de", "ja", "hi", "nl", "fr", "pl"];
+        const value = event.data.data();
+        const transcriptId = event.params.transcriptId;
+        const text = value.text ? value.text : value;
+        
         // All supported languages: https://cloud.google.com/translate/docs/languages
+        const languages = ["en", "es", "pt", "de", "ja", "hi", "nl", "fr", "pl"];
 
-        let from = value.language ? getLanguageWithoutLocale(value.language) : "en";
-        let promises = languages.map(to => {
-            console.log(`translating from '${from}' to '${to}', text '${text}'`);
+        const from = value.language ? getLanguageWithoutLocale(value.language) : "en";
+
+        const doc = {
+            timestamp: Firestore.FieldValue.serverTimestamp(),
+            languages: {}
+        }
+        
+        const promises = languages.map(to => {
             // Call the Google Cloud Platform Translate API
 
             if (from == to) {
-                return db.collection("translations").doc(transcriptId).set({to: {text: text, language: from}}, {merge: true});
+                return Promise.resolve().then(() => {
+                    doc.languages[from] = text;
+                });
             } else {
                 return translate.translate(text, {
-                    from: from,
-                    to: to
+                    from,
+                    to
                 }).then(result => {
-                    // Write the translation to the database
-                    let translation = result[0];
-                    return db.collection("translations").doc(transcriptId).set({to: {text: translation, language: to}}, {merge: true});
+                    const translation = result[0];
+                    return doc.languages[to] = translation;
                 });
             }
         });
-        return Promise.all(promises);
+        return Promise.all(promises).then(() => {
+            return db.collection("translations").doc(transcriptId).set(doc);
+        });
     });
